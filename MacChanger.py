415fbbbcd5b28ea79e5c3ebba3b6ccfed047d22b
Mohmed_Elsayed <40538784+mohmedelsayed@users.noreply.github.com>
2018-08-26 23:39:39 +0200

    Update MacChanger.py

--- MacChanger.py
+++ MacChanger.py
@@ -5,28 +5,50 @@
 # #----- Created By : Eng-Mohmed Elsayed ----- #            #
 #                                                           #
 #############################################################
-
-print("""
-#############################################################
-# #----- Simple Mac Changer anay Interface In Linux ------# #
-#                                                           #
-# #----- Created By : Eng-Mohmed Elsayed ----- #            #
-#                                                           #
-#############################################################
-""")
 import subprocess
 import optparse
-parser=optparse.OptionParser()
-parser.add_option("-i","--interface",dest="interface",help=" Set interface to change It's MAC Address")
-parser.add_option("-m","--mac",dest="new_mac",help=" Set New MAC Address")
+import time
 
-(options,arguments) = parser.parse_args()
-interface=options.interface
-new_mac= options.new_mac
-print("[+]" +" changing MAC Address For The Interface \""+interface +"\"" " To : "+new_mac+"\n\n")
-subprocess.call(["sudo", "ifconfig", interface, "down"])
-i=subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["sudo", "ifconfig", interface, "up"])
+try:
+    subprocess.call(["clear"])
+    print("""
+    #############################################################
+    # #----- Simple Mac Changer anay Interface In Linux ------# #
+    #                                                           #
+    # #----- Created By : Eng-Mohmed Elsayed ----- #            #
+    #                                                           #
+    #############################################################
+    """)
+
+    enter= '-\n-\n-\n-'
+    len_word="You Must Wtite :==> python MacChanger.py -i [Your Interface as wlan0 or  eth0 or wlan1...etc] -m [New Mac But Sure That The Mac Is Valid]"
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help=" Set interface to change It's MAC Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help=" Set New MAC Address")
+    print(len_word)
+    print(enter)
+    (options, arguments) = parser.parse_args()
+    interface = options.interface
+    new_mac = options.new_mac
+    print("[+]" + " changing MAC Address For The Interface \"" + interface + "\"" " To : " + new_mac +enter)
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    i = subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
+except Exception as error:
+    print ("Please wait !")
+    time.sleep(3)
+    print("""
+    Some Error Found ===> """+str(error))
+    time.sleep(2)
+    print('Note')
+    print("----")
+    print("To Solve all Problem =====>")
+    time.sleep(2)
+    print(enter+"Make Sure That You Didn't write : ==>python MacChanger.py")
+    time.sleep(3)
+    print(enter+"You Must Wtite :==> python MacChanger.py -i [Your Interface as wlan0 or  eth0 or wlan1...etc] -m [New Mac But Sure That The Mac Is Valid]"+enter)
+
+
 
 
 
